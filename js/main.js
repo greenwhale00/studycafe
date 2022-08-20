cd16ed0b023e5deb94d9539f8a8924e09470de1c
greenwhale00 <105770998+greenwhale00@users.noreply.github.com>
2022-08-20 17:40:26 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $('.main_slider').slick({
+        arrows: false,
+        //autoplay: true,
+        dots: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+
+
+})
+
+
